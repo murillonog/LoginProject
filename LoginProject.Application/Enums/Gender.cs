e26f4c7868d6be5123c706ec23429810dd216808
Murillo Nogueira <murillo.nogueira01@outlook.com>
2023-04-10 22:47:08 -0300

    Adicionar arquivos de projeto.

---
+++ LoginProject.Application/Enums/Gender.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace LoginProject.Application.Enums
+{
+    public enum Gender
+    {
+        [Description("Male")]
+        Male = 0,
+        [Description("Female")]
+        Female = 1,
+        [Description("Others")]
+        Others = 2,
+    }
+}
---
+++ LoginProject.WebUI/ViewModels/LoginViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using System.ComponentModel.DataAnnotations;
+
+namespace LoginProject.WebUI.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid format email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(20, ErrorMessage = "The {0} must be at least {2} and at max " +
+            "{1} characters long.", MinimumLength = 10)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        public string? ReturnUrl { get; set; }
+        public string? Provider { get; set; }
+        public IEnumerable<AuthenticationScheme>? Providers { get; set; }
+    }
+}
