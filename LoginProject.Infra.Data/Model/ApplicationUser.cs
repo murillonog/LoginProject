313ff5a7dcbc90069dabfa92f259aeec25b113ad
Murillo Nogueira <murillo.nogueira01@outlook.com>
2023-04-10 23:04:37 -0300

    Create ContextDb

---
+++ LoginProject.Infra.Data/Model/ApplicationUser.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace LoginProject.Infra.Data.Model
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime Birth { get; set; }
+        public string Address { get; set; }
+        public int Gender { get; set; }
+        public int Age { get; set; }
+
+        public ApplicationUser GetUserDefault(string name, string email)
+          => new()
+          {
+              UserName = email,
+              Name = name,
+              Email = email,
+              NormalizedUserName = email.ToUpper(),
+              NormalizedEmail = email.ToUpper(),
+              Address = "Address Default",
+              Birth = new DateTime(2000, 1, 1),
+              Gender = 3,
+              PhoneNumber = "11111111111",
+              Age = DateTime.Now.Subtract(new DateTime(2000, 1, 1)).Days / 365,
+              EmailConfirmed = true,
+              LockoutEnabled = false,
+              SecurityStamp = Guid.NewGuid().ToString()
+          };
+    }
+}
