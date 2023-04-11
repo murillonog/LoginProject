645235e1277a777a5ed42bd59e4073619ebf1f1e
Murillo Nogueira <murillo.nogueira01@outlook.com>
2023-04-10 22:55:47 -0300

    Create Views

---
+++ LoginProject.WebUI/Extensions/MapExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace LoginProject.WebUI.Extensions
+{
+    public static class MapExtensions
+    {
+        public static int GetAge(this DateTime birthDay)
+            => DateTime.Now.Subtract(birthDay).Days % 365 == 0 ?
+                DateTime.Now.Subtract(birthDay).Days / 365 : (DateTime.Now.Subtract(birthDay).Days / 365) + 1;
+    }
+}
